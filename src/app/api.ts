c81bf743653cf1b927ceaea7b8b8d1a38adc8acc
Vanessa Cassandra <vanessa.cassandra@juliusbaer.com>
2021-07-23 20:32:42 +0800

    Change API URL to https

--- src/app/api.ts
+++ src/app/api.ts
@@ -1,10 +1,10 @@
 const api = {
   search: (query: string, page: number) =>
-    fetch(`http://www.omdbapi.com/?apikey=b9bd48a6&s=${query}&page=${page}&type=movie`).then((res) =>
+    fetch(`https://www.omdbapi.com/?apikey=b9bd48a6&s=${query}&page=${page}&type=movie`).then((res) =>
       res.json()
     ),
   getDetails: (imdbID: string) =>
-    fetch(`http://www.omdbapi.com/?apikey=b9bd48a6&i=${imdbID}`).then((res) => res.json()),
+    fetch(`https://www.omdbapi.com/?apikey=b9bd48a6&i=${imdbID}`).then((res) => res.json()),
 };
 
 export default api;
